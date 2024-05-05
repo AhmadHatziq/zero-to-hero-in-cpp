28f5a7a5f7e823e780a6eb40cca0206a68e7de7c
Hatziq <hatziq@u.nus.edu>
2024-05-05 10:25:43 +0800

    Add comment

--- getting-started/get-name.cpp
+++ getting-started/get-name.cpp
@@ -6,7 +6,7 @@ int main()
 {
     string yourName;
     cout << "Enter your name: ";
-    getline(cin, yourName);
+    getline(cin, yourName); // Can be used for multi-spaced input
     cout << "Hello " << yourName << endl; // Printing User's name with Hello
     cout << "Welcome to Educative" << endl;
     return 0;
