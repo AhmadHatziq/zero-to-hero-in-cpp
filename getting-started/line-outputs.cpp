ecd30075dfd358571d447dab8e9a4babca0beb00
Hatziq <hatziq@u.nus.edu>
2024-05-05 10:11:40 +0800

    Add line output example

---
+++ getting-started/line-outputs.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  cout << "Single"; // The next output will be on the same line 
+  cout << "Line\n" ;  // The next output will be on the next line due to new line symbol \n 
+  cout << "Separate new line" << "\n"; // The new line symbol can be written separately
+  cout << "Use of endl for new line" << endl; // The endl must be written separately
+  cout << "It demonstrates the line break"; // This line is shown on a new line
+  return 0;
+}
